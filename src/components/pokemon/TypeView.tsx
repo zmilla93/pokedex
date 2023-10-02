ce23a7c6a64638818792baabf99661f6eac8044f
Zach Miller <zmilla93@gmail.com>
2023-10-02 17:31:09 -0500

    added colors for all pokemon types

--- src/components/pokemon/TypeView.tsx
+++ src/components/pokemon/TypeView.tsx
@@ -1,9 +1,13 @@
+
 export function TypeView({ types }: { types: string[] }) {
     const typeJsx = types.map((entry) => {
-        // const type = entry.type.name;
-        const typeColor = getTypeColors(entry);
+        const typeColor = "#" + typeColors[entry];
         return (
-            <div key={entry} className={typeColor.out() + "inline-flex rounded border border-white-800 py-1 text-sm text-white w-20 justify-center"}>
+            <div key={entry}
+                className={"inline-flex rounded border border-white-800 py-1 text-sm text-white w-20 justify-center"}
+                // Since tailwind can't handle string concatenation, the default style attribute is used for type colors
+                style={{ backgroundColor: typeColor }}
+            >
                 {entry.toUpperCase()}
             </div>
         );
@@ -16,20 +20,26 @@ export function TypeView({ types }: { types: string[] }) {
     );
 }
 
-function getTypeColors(type: string): TypeColor {
-    switch (type) {
-        case 'fire':
-            return new TypeColor("bg-orange-600", "text-black");
-        default:
-            return new TypeColor("bg-black", "white");
-    }
-}
-
-class TypeColor {
-    constructor(public background: string, public text?: string) {
-
-    }
-    out() {
-        return this.background + " " + this.text + " ";
-    }
-}
+// Colors from https://bulbapedia.bulbagarden.net/wiki/Category:Type_color_templates
+const typeColors: { [index: string]: string } = {
+    bug: "A8B820",
+    dark: "705848",
+    dragon: "7038F8",
+    electric: "F8D030",
+    fairy: "EE99AC",
+    fighting: "C03028",
+    fire: "F08030",
+    flying: "A890F0",
+    ghost: "705898",
+    grass: "78C850",
+    ground: "E0C068",
+    ice: "98D8D8",
+    normal: "A8A878",
+    poison: "A040A0",
+    psychic: "F85888",
+    rock: "B8A038",
+    shadow: "#1f1f1f",
+    steel: "B8B8D0",
+    water: "6890F0",
+    unknown: "68A090",
+};
