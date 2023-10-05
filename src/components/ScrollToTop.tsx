475d74089ff13b97abed6c13864728bd2633bef2
Zach Miller <zmilla93@gmail.com>
2023-10-05 12:24:17 -0500

    removed log

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -3,7 +3,6 @@ import { useLocation } from "react-router-dom";
 
 export function ScrollToTop() {
     const { pathname } = useLocation();
-    console.log(location);
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
