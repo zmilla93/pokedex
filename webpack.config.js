4243cea7be9669226c640e939227d569698d240f
Zach Miller <zmilla93@gmail.com>
2023-10-09 16:02:55 -0500

    Revert "favicon fix for github pages"
    This reverts commit 6ba9940ab8f8646181082a5596502a38e633bd5c.

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,7 @@ const config = {
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: staticInputFolder + '/index.html',
+            favicon: staticInputFolder + '/favicon.png',
             // publicPath: '/',
             hash: false,
         }),
