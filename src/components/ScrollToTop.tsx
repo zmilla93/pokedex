b81cde09fab9f2e0e6bdaf9abd77e8979c81e10e
Zach Miller <zmilla93@gmail.com>
2023-10-10 01:53:38 -0500

    fixed ScrollToTop by allowing it to scroll specific divs

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,10 +1,14 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-export function ScrollToTop() {
+export function ScrollToTop({ targetRef }: { targetRef: React.RefObject<HTMLDivElement> }) {
     const { pathname } = useLocation();
     useEffect(() => {
         window.scrollTo(0, 0);
-    }, [pathname]);
+        if (targetRef != null && targetRef.current != null) {
+            targetRef.current.scrollTo(0, 0);
+            console.log("GO");
+        }
+    }, [pathname, targetRef]);
     return null;
 }
--- src/pages/PageWrapper.tsx
+++ src/pages/PageWrapper.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { ScrollToTop } from "../components/ScrollToTop";
 import { Navbar } from "../components/Navbar";
+import { useRef } from "react";
 
 export function PageWrapper() {
+    const scrollRef = useRef<HTMLDivElement>(null);
     return (
         <div id="pageWrapper" className="flex flex-col h-full">
-            <ScrollToTop />
             <div id="header" className="flex flex-grow-0 flex-shrink">
                 <Navbar />
             </div>
             <div id="pageContent"
-                className="flex flex-col flex-grow flex-shrink overflow-auto items-center bg-blue-200">
+                className="flex flex-col flex-grow flex-shrink overflow-auto items-center bg-blue-200"
+                ref={scrollRef}>
+                <ScrollToTop targetRef={scrollRef} />
                 <div id="contentWrapper" className=" max-w-5xl bg-orange-200">
                     <Outlet />
                 </div>
